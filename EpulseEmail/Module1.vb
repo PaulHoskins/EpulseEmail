e83e8e66c847bac6e0f869a5c68164627437b0d4
PaulHoskins <pa_hoskins@hotmail.com>
2017-04-20 07:44:40 +0100

    Initial

--- EpulseEmail/Module1.vb
+++ EpulseEmail/Module1.vb
@@ -1,7 +1,22 @@
 ﻿Module Module1
+    Dim server, email, password As String
 
     Sub Main()
+        setup()
+        Console.WriteLine("EpulseEmail version 1.00.00 20/04/2017")
+        Console.WriteLine("Connect To {0} {1}", server, email)
+
+
+
+        MsgBox(server)
 
     End Sub
 
+    Sub setup()
+
+        server = "imap-mail.outlook.com"
+        email = "paulanhoskins@outlook.com"
+        password = "00pudsey"
+
+    End Sub
 End Module
